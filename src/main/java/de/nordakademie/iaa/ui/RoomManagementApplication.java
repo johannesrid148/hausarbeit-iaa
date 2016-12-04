95f362d6a0db5937f7c7b03c5819e902ef9298ae
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2016-12-04 17:19:53 +0100

    [ANGULAR]: we do not even need to add classes manually...

--- src/main/java/de/nordakademie/iaa/ui/RoomManagementApplication.java
+++ src/main/java/de/nordakademie/iaa/ui/RoomManagementApplication.java
@@ -1,18 +1,9 @@
 package de.nordakademie.iaa.ui;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api")
 public class RoomManagementApplication extends Application {
 
-  @Override
-  public Set<Class<?>> getClasses() {
-    final Set<Class<?>> classes = new HashSet<>(super.getClasses());
-    classes.add(RoomController.class);
-    return classes;
-  }
 }
