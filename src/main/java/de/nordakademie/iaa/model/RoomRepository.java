bf1f97a3bf088280a733801fc75f7e4317a37963
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2017-04-15 12:43:37 +0200

    Add logging

--- src/main/java/de/nordakademie/iaa/model/RoomRepository.java
+++ src/main/java/de/nordakademie/iaa/model/RoomRepository.java
@@ -5,6 +5,9 @@ import static java.util.Collections.synchronizedList;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class RoomRepository {
 
   private final List<Room> rooms = synchronizedList(new ArrayList<Room>() {{
@@ -22,6 +25,7 @@ public class RoomRepository {
   }
 
   public List<Room> readAll() {
+    LOG.info("Read all rooms");
     return rooms;
   }
 
--- src/main/webapp/js/resource/roomResource.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-application.factory('Room', function () {
-  return function (building, number) {
-    this.building = building;
-    this.number = number;
-  }
-});
