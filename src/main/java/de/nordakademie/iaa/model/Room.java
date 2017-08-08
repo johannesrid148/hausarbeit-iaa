46fbab8e2526a0375ae13f468b65f89f5d173262
Benedikt Stemmildt <benedikt@stemmildt.com>
2017-08-08 20:34:26 +0200

    Remove lombok

--- src/main/java/de/nordakademie/iaa/model/Room.java
+++ src/main/java/de/nordakademie/iaa/model/Room.java
@@ -1,14 +1,28 @@
 package de.nordakademie.iaa.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class Room {
 
   private String building;
   private String number;
+
+  public Room(final String building, final String number) {
+    this.building = building;
+    this.number = number;
+  }
+
+  public String getBuilding() {
+    return building;
+  }
+
+  public void setBuilding(final String building) {
+    this.building = building;
+  }
+
+  public String getNumber() {
+    return number;
+  }
+
+  public void setNumber(final String number) {
+    this.number = number;
+  }
 }
