98dffcb18e42e49b09c6e0a45860c1a221d36fe1
Benedikt Stemmildt <benedikt@stemmildt.com>
2017-08-30 09:32:13 +0200

    [DATABASE] Add entityManagerFactory to our application context

--- src/main/java/de/nordakademie/iaa/Context.java
+++ src/main/java/de/nordakademie/iaa/Context.java
@@ -1,10 +1,39 @@
 package de.nordakademie.iaa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
 import de.nordakademie.iaa.model.RoomRepository;
 import de.nordakademie.iaa.service.RoomService;
 
-public class Context {
+@WebListener
+public class Context implements ServletContextListener {
 
   public static final RoomRepository ROOM_REPOSITORY = new RoomRepository();
   public static final RoomService ROOM_SERVICE = new RoomService();
+
+  private static EntityManagerFactory entityManagerFactory;
+
+  public static EntityManager createEntityManager() {
+    if (entityManagerFactory == null) {
+      throw new IllegalStateException("ServletContext is not initialized yet.");
+    }
+    return entityManagerFactory.createEntityManager();
+  }
+
+  @Override
+  public void contextInitialized(final ServletContextEvent servletContextEvent) {
+    entityManagerFactory = Persistence.createEntityManagerFactory("IAA_DB");
+    System.out.println("<------------ ServletContext initialized ------------>");
+  }
+
+  @Override
+  public void contextDestroyed(final ServletContextEvent servletContextEvent) {
+    entityManagerFactory.close();
+    System.out.println("<------------ ServletContext destroyed ------------>");
+  }
 }
