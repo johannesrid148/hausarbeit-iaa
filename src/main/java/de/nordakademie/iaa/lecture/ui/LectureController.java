9cc5ab2ecbe152c4463e0a6a00d641d8d75e569d
Benedikt Stemmildt <benedikt@stemmildt.com>
2017-09-26 16:35:25 +0200

    Fix RequestBody

--- src/main/java/de/nordakademie/iaa/lecture/ui/LectureController.java
+++ src/main/java/de/nordakademie/iaa/lecture/ui/LectureController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class LectureController {
   }
 
   @PostMapping
-  public Lecture saveLecture(final Lecture lecture) {
+  public Lecture saveLecture(@RequestBody final Lecture lecture) {
     return lectureService.create(lecture);
   }
 }
--- src/main/java/de/nordakademie/iaa/room/ui/RoomController.java
+++ src/main/java/de/nordakademie/iaa/room/ui/RoomController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +29,7 @@ public class RoomController {
   }
 
   @PostMapping
-  public Room saveRoom(final Room room) {
+  public Room saveRoom(@RequestBody final Room room) {
     return roomService.create(room);
   }
 }
